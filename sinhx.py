26e50725aa7cd97cb14853c964b806099d4386c8
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2020-05-08 23:16:27 +0530

    Update sinhx.py

--- sinhx.py
+++ sinhx.py
@@ -2,9 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt 
 x=float(input())
 y=np.sinh(x) 
-in_val = np.linspace(-np.pi, np.pi, 12)
-out_val = np.sinh(in_val) 
 print("np.sinh (",x,"): ", y)
+in_val = []
+
+in_val.append(-np.pi)
+in_val.append(-np.pi/2)
+in_val.append(-np.pi/3)
+in_val.append(-np.pi/6)
+in_val.append(0)
+in_val.append(np.pi/6)
+in_val.append(x)
+in_val.append(np.pi/3)
+in_val.append(np.pi/2)
+in_val.append(np.pi)
+
+in_val=sorted(in_val)
+out_val=np.sinh(in_val)
+
+ 
 plt.plot(in_val, out_val, color = 'red', marker = "o")
 plt.plot(x,y,color = 'blue', marker = "o")
 plt.title("y=sinh(x)") 
