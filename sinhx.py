6d9646d4c169cd7382970798410028daab1f81ad
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2020-05-09 01:22:36 +0530

    Update sinhx.py

--- sinhx.py
+++ sinhx.py
@@ -2,27 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt 
 x=float(input())
 y=np.sinh(x) 
-print("np.sinh (",x,"): ", y)
+print("sinh (",x,"): ", y)
 in_val = []
-
-in_val.append(-np.pi)
-in_val.append(-np.pi/2)
-in_val.append(-np.pi/3)
-in_val.append(-np.pi/6)
+for i in range(1,int(x+10),2):
+    in_val.append(-np.pi/i)
 in_val.append(0)
-in_val.append(np.pi/6)
 in_val.append(x)
-in_val.append(np.pi/3)
-in_val.append(np.pi/2)
-in_val.append(np.pi)
-
+for i in range(1,int(x+10),2):
+    in_val.append(np.pi/i)
 in_val=sorted(in_val)
 out_val=np.sinh(in_val)
-
- 
-plt.plot(in_val, out_val, color = 'red', marker = "o")
+plt.plot(in_val, out_val, color = 'red', marker = "o",)
 plt.plot(x,y,color = 'blue', marker = "o")
+plt.annotate("(0,0)", (-0.5, 0.7))
+plt.axis([int(x)-10, int(x)+10, int(y)-30, int(y)+10])
 plt.title("y=sinh(x)") 
+plt.hlines(y=0, xmin=int(x)-10, xmax=0)
+plt.vlines(x=0, ymin=int(y)-30, ymax=0)
 plt.xlabel("X") 
-plt.ylabel("Y") 
+plt.ylabel("Y")
+plt.grid()
 plt.show() 
